b812fb198ac35a1e2573528efdd4786776f27bc9
vince <vincent.harpin@orange.fr>
2022-12-21 21:30:06 +0100

    exercice 120

---
+++ methode.js
@@ -0,0 +1,251 @@
+//Bouton plein
+$("#plein").click(function () {
+    $(".carre").css("background-color", "red");
+    $(".cercle").css("background-color", "red");
+    $(".losange").css("background-color", "red");
+})
+
+//Bouton vide
+$("#vide").click(function () {
+    $(".carre").css("background-color", "white");
+    $(".cercle").css("background-color", "white");
+    $(".losange").css("background-color", "white");
+})
+
+//Fonction des carrés
+$(".carre1").click(function () {
+    let forme = $(".carre1").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+        $(".bloc1").css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+        $(".bloc1").css("background-color", "red");
+    }
+})
+
+$(".carre2").click(function () {
+    let forme = $(".carre2").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+        $(".bloc2").css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+        $(".bloc2").css("background-color", "red");
+    }
+})
+
+$(".carre3").click(function () {
+    let forme = $(".carre3").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+        $(".bloc3").css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+        $(".bloc3").css("background-color", "red");
+    }
+})
+
+$(".carre4").click(function () {
+    let forme = $(".carre4").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+        $(".bloc4").css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+        $(".bloc4").css("background-color", "red");
+    }
+})
+
+//Fonction du losange
+$(".carre5").click(function () {
+    let forme = $(".carre5").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+        $(".bloc5").css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+        $(".bloc5").css("background-color", "red");
+    }
+})
+
+//Fonction des cercles
+$(".site1").click(function () {
+    let forme = $(".site1").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site2").click(function () {
+    let forme = $(".site2").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site3").click(function () {
+    let forme = $(".site3").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site4").click(function () {
+    let forme = $(".site4").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site5").click(function () {
+    let forme = $(".site5").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site6").click(function () {
+    let forme = $(".site6").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site7").click(function () {
+    let forme = $(".site7").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site8").click(function () {
+    let forme = $(".site8").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site9").click(function () {
+    let forme = $(".site9").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site10").click(function () {
+    let forme = $(".site10").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site11").click(function () {
+    let forme = $(".site11").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site12").click(function () {
+    let forme = $(".site12").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site13").click(function () {
+    let forme = $(".site13").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site14").click(function () {
+    let forme = $(".site14").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site15").click(function () {
+    let forme = $(".site15").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site16").click(function () {
+    let forme = $(".site16").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site17").click(function () {
+    let forme = $(".site17").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site18").click(function () {
+    let forme = $(".site18").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site19").click(function () {
+    let forme = $(".site19").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
+
+$(".site20").click(function () {
+    let forme = $(".site20").css("background-color");
+    if(forme === "rgb(255, 0, 0)") {
+        $(this).css("background-color", "white");
+    } else {
+        $(this).css("background-color", "red");
+    }
+})
